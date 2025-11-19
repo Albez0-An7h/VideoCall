f552ac607ca6ab635866211ff46a518761a8a535
Albez0-An7h <ansh.kumar7096@gmail.com>
2025-11-19 10:04:26 +0530

    fixing redirection

--- client/VideoCall/src/providers/Socket.jsx
+++ client/VideoCall/src/providers/Socket.jsx
@@ -8,7 +8,7 @@ export const useSocket = () => {
 }
 
 const SocketProvider = (props) => {
-    const socket = useMemo(() => io('http://localhost:8000'), [])
+    const socket = useMemo(() => io(import.meta.env.VITE_SERVER_URL || 'http://localhost:8000'), [])
 
     return (
         <SocketContext.Provider value={{ socket }}>
